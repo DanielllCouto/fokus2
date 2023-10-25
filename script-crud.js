5a311d13168955d0290295f3c3a3cf823e18dd1b
Daniel Estrella Couto <estrellacouto05@gmail.com>
2023-10-25 13:08:35 -0300

    Update script-crud.js

--- script-crud.js
+++ script-crud.js
@@ -7,13 +7,14 @@ const formLabel = document.querySelector('.app__form-label')
 const textarea = document.querySelector('.app__form-textarea')
 
 const cancelFormTaskBtn = document.querySelector('.app__form-footer__button--cancel')
+
+const taskAtiveDescription = document.querySelector('.app__section-active-task-description')
+
 const btnCancelar = document.querySelector('.app__form-footer__button--cancel')
 
+const localStorageTarefas = localStorage.getItem('tarefas')
 
-
-let tarefas = [
-]
-
+let tarefas = localStorageTarefas ? JSON.parse(localStorageTarefas) : []
 
 const taskIconSvg = `
 <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
@@ -24,10 +25,49 @@ const taskIconSvg = `
         fill="#01080E" />
 </svg>
 `
+let tarefaSelecionada = null
+let itemTarefaSelecionada = null
+
+let tarefaEmEdicao = null
+let paragraphEmEdicao = null
+
+const selecionaTarefa = (tarefa, elemento) => {
+
+    document.querySelectorAll('.app__section-task-list-item-active').forEach(function (button) {
+        button.classList.remove('app__section-task-list-item-active')
+    })
+    
+    if (tarefaSelecionada == tarefa) {
+        taskAtiveDescription.textContent = null
+        itemTarefaSelecionada = null
+        tarefaSelecionada = null
+        return
+    }
+    
+    tarefaSelecionada = tarefa
+    itemTarefaSelecionada = elemento
+    taskAtiveDescription.textContent = tarefa.descricao
+    elemento.classList.add('app__section-task-list-item-active')
+    }
 
 const limparForm = () => {
+    tarefaEmEdicao = null
+    paragraphEmEdicao = null
     textarea.value = ''
     formTask.classList.add('hidden')
+}
+
+const selecionaTarefaParaEditar = (tarefa, elemento) => {
+    if (tarefaEmEdicao == tarefa) {
+        limparForm()
+        return
+    }
+    
+    formLabel.textContent='Editando tarefa'
+    tarefaEmEdicao = tarefa
+    paragraphEmEdicao = elemento
+    textarea.value = tarefa.descricao
+    formTask.classList.remove('hidden')
 }
 
 function createTask(tarefa) {
@@ -42,8 +82,40 @@ function createTask(tarefa) {
 
     paragraph.textContent = tarefa.descricao
 
+    const button = document.createElement('button')
+
+    button.classList.add('app_button-edit')
+
+    const editIcon = document.createElement('img')
+    editIcon.setAttribute('src',"./imagens/edit.png" )
+
+    button.appendChild(editIcon)
+
+    button.addEventListener('click', (event) =>{
+        event.stopPropagation()
+        selecionaTarefaParaEditar (tarefa, paragraph)
+    })
+
+
+    li.onclick = () => {
+        selecionaTarefa(tarefa,li)
+    }
+
+    svgIcon.addEventListener('click', (event) => {
+        event.stopPropagation()
+        button.setAttribute('disabled', true)
+        li.classList.add('app__section-task-list-item-complete')
+    })
+
+    if(tarefa.concluida){
+        button.setAttribute('disabled', true)
+        li.classList.add('app__section-task-list-item-complete')
+    }
+
     li.appendChild(svgIcon)
     li.appendChild(paragraph)
+    li.appendChild(button)
+
     
     return li
 }
@@ -64,8 +136,16 @@ toggleFormTaskBtn.addEventListener('click', () => {
     formTask.classList.toggle('hidden')
 })
 
+const updateLocalStorage = () =>  {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+}
+
 formTask.addEventListener('submit', (evento) => {
     evento.preventDefault()
+    if(tarefaEmEdicao) {
+        tarefaEmEdicao.descricao = textarea.value
+        paragraphEmEdicao.textContent = textarea.value
+    }else{
     const task = {
         descricao: textarea.value,
         concluida: false
@@ -73,7 +153,8 @@ formTask.addEventListener('submit', (evento) => {
     tarefas.push(task)
     const taskItem = createTask(task)
     taskListContainer.appendChild(taskItem)
-
+    }
+    updateLocalStorage()
     limparForm()
 })
 
